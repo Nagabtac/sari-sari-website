e994e0b31bd843fb6c1c6145d0961234393aa49e
Nagabtac <darwin.catbagan@lorma.edu>
2025-11-19 09:22:35 +0800

    files for req

---
+++ src/test/java/com/sari_sari/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sari_sari.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
