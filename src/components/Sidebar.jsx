50d40c529a4dd231a0cd35c664cd3c1a22bc1467
Nagabtac <darwin.catbagan@lorma.edu>
2025-11-27 12:19:26 +0800

    kh

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ function Sidebar({ status }) {
   const menuItems = [
     { 
       icon: (
-       <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M1 6V15H6V11C6 9.89543 6.89543 9 8 9C9.10457 9 10 9.89543 10 11V15H15V6L8 0L1 6Z" fill="#ffffffff"></path> </g></svg>
+      <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M1 6V15H6V11C6 9.89543 6.89543 9 8 9C9.10457 9 10 9.89543 10 11V15H15V6L8 0L1 6Z" fill="#ffffffff"></path> </g></svg>
 
         
       ), 
@@ -34,8 +34,8 @@ function Sidebar({ status }) {
   ]
 
   return (
-    <div className="sidebar" style={{ display: status ? 'flex' : 'none' }}>
-      <div className="sidebar-title">FrozenPOS</div>
+    <div className="sidebar"  style={{ display: status ? 'flex' : 'none' }}>
+      <div className="sidebar-title">Sari-Sari Store</div>
       <nav>
         <ul className="sidebar-nav">
           {menuItems.map((item, index) => (
